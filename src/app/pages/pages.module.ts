23ec3ffe8c26c384c4c0bde7362df5d411c4f987
amir-rhmi <PKim@genedx.com>
2023-12-26 16:30:17 +0330

    feat: course detail and course changes

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PagesComponent } from './pages.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from '../shared/components/header/header.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    children:[
+      {
+        path: '',
+        redirectTo: 'courses',
+        pathMatch: 'full'
+      },
+      {
+        path: 'courses',
+        loadChildren: () => import('./courses/courses.module').then((m) => m.CoursesModule)
+      }
+    ]
+  }
+]
 
+const CUSTOM = [
+  HeaderComponent
+]
 
 @NgModule({
-  declarations: [],
+  declarations: [PagesComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    ...CUSTOM
   ]
 })
 export class PagesModule { }
